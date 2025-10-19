e353038fa79e7dd3fd01182887dc73ade831c91d
Ludurn <luigifdsn@hotmail.com>
2025-10-19 16:17:16 -0300

    need to merge this later

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     
   </nav>
-  <transition name="scroll" mode="out-in" >
+  <transition :name="transitionName" mode="out-in" >
     <router-view :key="$route.fullPath" />
   </transition>
   <transition name="fade" mode="default">
@@ -12,8 +12,17 @@
 
 <script setup lang="ts">
 import ZoomProp from '@/components/ZoomProp.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
 import { useNoDragImages } from './composables/useNoDragImages'
+
+const route = useRoute()
+
+const transitionName = computed(() => {
+  if (route.path.startsWith('/dream')) return 'fade'
+  return 'scroll'
+})
 
 useNoDragImages()
 
--- client/src/views/DreamView.vue
+++ client/src/views/DreamView.vue
@@ -3,12 +3,31 @@
     <StarField/>
     <div id="content">
         <h1>OLÁ</h1>
+        <div id="sprite">
+          <img :src="sprite" @click="playAnimation" alt="">
+        </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import StarField from '@/components/StarField.vue'
+import { ref } from 'vue'
+
+import image1 from '@/assets/imgs/cat.jpg'
+import image2 from '@/assets/imgs/fachada.png'
+import image3 from '@/assets/imgs/turma-25.png'
+
+const sprites = [image1, image2, image3]
+const sprite = ref(image1)
+
+function playAnimation() {
+  let i = 0
+  const interval = setInterval(() => {
+    sprite.value = sprites[i]
+    i++
+    if (i >= sprites.length) clearInterval(interval)
+  }, 250)
+}
 
 </script>
-
