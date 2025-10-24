e31e7d65d319f888e4f3afc50102fddec770a691
luigi duran <luigifdsn@hotmail.com>
2025-10-23 23:16:01 -0300

    feat: set mask to avatar images and style to the text of dream page

--- client/src/views/DreamView.vue
+++ client/src/views/DreamView.vue
@@ -2,32 +2,32 @@
   <div id="dream" ref="page">
     <StarField/>
     <div id="content">
-        <h1 class="title-100">NOSSO SONHO</h1>
-        <p class="paragraph-40">
-          Nós não estamos apenas terminando mais um ano mas, também, um ciclo. Assim, sonhamos com um futuro onde
-          o fim dessa etapa inaugurará o início de muitas outras.
-        </p>
-        <img :src="graduationCap"/>
-        <div id="filter">
-          <div @click="filter_job(area)" v-for="area in areas" :key="area" class="btns-area">
-            <p> {{ area.title }} </p>
-          </div>
+      <h1 class="title-100">NOSSO SONHO</h1>
+      <p class="paragraph-40 p-dream">
+        Nós não estamos apenas terminando mais um ano mas, também, um ciclo. Assim, sonhamos com um futuro onde
+        o fim dessa etapa inaugurará o início de muitas outras.
+      </p>
+      <img :src="graduationCap"/>
+      <div id="filter">
+        <div @click="filter_job(area)" v-for="area in areas.filter(a => a.title !== 'Default')" :key="area.title" :class="['btns-area', area.title[0], { 'pressed': selectedArea === area.title }]">
+          <p> {{ area.title }} </p>
         </div>
-        <transition name="fade" mode="default">
-          <div v-show="showGallery" id="avatar-gallery">
-            <div id="area-label">
-              <p class="paragraph-40">
-               {{ areaDesc }}
-              </p>
+      </div>
+      <div id="avatar-gallery" :class="{ 'fade-out': !showGallery, 'fade-in': showGallery }">
+        <div id="area-label">
+          <p class="paragraph-40 p-dream">
+            {{ areaDesc }}
+          </p>
+        </div>
+        <div id="avatar-photos">
+          <figure v-for="avatar in filteredAvatars" :key="avatar" class="avatars-fig">
+            <div class="mask-wrapper">
+            <img :src="avatar['img']" alt="">
             </div>
-            <div id="avatar-photos">
-              <figure v-for="avatar in filteredAvatars" :key="avatar" class="avatars-fig">
-                <img :src="avatar['img']" alt="">
-                <p>{{ avatar['job'] }}</p>
-              </figure>
-            </div>
-          </div>
-        </transition>
+            <p class="avatar-job">{{ avatar['job'] }}</p>
+          </figure>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -63,9 +63,13 @@ const areas: area[] = [
     text: "Comunicação e Marketing são áreas estratégicas que utilizam criatividade, "
     + "planejamento e análise para posicionar marcas, divulgar produtos e fortalecer "
     + "relacionamentos com o público."
+  },
+  {
+    title: "Default",
+    text: "\"Por vezes acordamos. Por vezes caimos. E por vezes, quando caimos, voamos.\" -Sandman"
   }
 ]
-const areaDesc = ref('')
+const areaDesc = ref<string>(areas[3].text)
 const avatars = ref(avatarsData)
 const selectedArea = ref<string | null>(null)
 const showGallery = ref(true)
@@ -81,7 +85,7 @@ function filter_job(area: area) {
   showGallery.value = false
 
   setTimeout(() => {
-    areaDesc.value = (selectedArea.value == area.title) ? null : area.text
+    areaDesc.value = (selectedArea.value == area.title) ? areas[3].text : area.text
     selectedArea.value = (selectedArea.value == area.title) ? null : area.title
   }, 1000);
 }
